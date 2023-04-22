5f1e18daeaeba45cc3b1b9479f5be4a20bc864db
IzmYuta <y.izumi@4nonome.com>
2023-04-23 03:13:55 +0900

    Add engine

--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ POSTGRES_DB = os.environ.get("POSTGRES_DB")
 SQLALCHEMY_DATABASE_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
     POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_PORT, POSTGRES_DB
 )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 Base = declarative_base()
