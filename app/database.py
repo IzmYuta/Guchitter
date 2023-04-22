766196fe11646c3e9c3d3ab255177c1365f593d5
IzmYuta <y.izumi@4nonome.com>
2023-04-23 03:20:57 +0900

    Add env

--- app/database.py
+++ app/database.py
@@ -3,14 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("PGUSER")
+POSTGRES_PASSWORD = os.environ.get("PGPASSWORD")
+POSTGRES_HOST = os.environ.get("PGHOST")
+POSTGRES_PORT = os.environ.get("PGPORT")
+POSTGRES_DB = os.environ.get("PGDATABASE")
 
 SQLALCHEMY_DATABASE_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
-    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_PORT, POSTGRES_DB
+    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB
 )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
