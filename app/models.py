c5de270f92451fbbf35d6af14b2d62dcabc5e80a
IzmYuta <y.izumi@4nonome.com>
2023-04-23 04:44:53 +0900

    change model

--- app/models.py
+++ app/models.py
@@ -5,5 +5,5 @@ from .database import Base
 
 class Guchi(Base):
     __tablename__ = "guchi"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     body = Column(String)
