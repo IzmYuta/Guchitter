ce5e89fcf93b344be19ca92da69c758979264b41
IzmYuta <y.izumi@4nonome.com>
2023-04-25 20:49:50 +0900

    Add SSL keys(localhost)

--- app/database.py
+++ app/database.py
@@ -3,6 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
 POSTGRES_USER = os.environ.get("PGUSER")
 POSTGRES_PASSWORD = os.environ.get("PGPASSWORD")
 POSTGRES_HOST = os.environ.get("PGHOST")
